2bc2f74423c790d1561f6ff04aa95d4edc7139bc
Epicsociety <measkswho@gmail.com>
2023-05-22 21:38:42 +0300

    json file created

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Scipt use the REST API to retrive info about employee and write on a json file
+"""
+
+import csv
+import json
+import requests
+import sys
+import urllib3
+
+# Disable the InsecureRequestWarning
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+def employee_info_to_json(userId):
+    """ get employee todo list, get all tasks and write on a file
+    Args (int)
+        userId
+    Returns: file
+    """
+
+    employee = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(userId), verify=False).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(userId), verify=False).json()
+
+    todolist = []
+    for todo in todos:
+        mydict = {}
+        mydict['tasks'] = todo.get("title")
+        mydict['complete'] = todo.get("completed")
+        mydict['username'] = employee.get("username")
+        todolist.append(mydict)
+
+    json_obj = {}
+    json_obj[userId] = todolist
+
+    with open("{}.json".format(userId), 'w') as jsonfile:
+        json.dump(json_obj, jsonfile)
+
+
+if __name__ == '__main__':
+    userId = int(sys.argv[1])
+    employee_info_to_json(userId)
