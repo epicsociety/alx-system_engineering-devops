ec773e9b04f6e78edf84d5d9bb9b0110bfbf6cc9
Epicsociety <measkswho@gmail.com>
2023-05-23 10:54:32 +0300

    improve the code

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -19,16 +19,17 @@ def get_employee_and_tasks(userId):
     Returns: file
     """
 
-    employee_name = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                                 .format(userId), verify=False).json()
+    emp_name = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(userId), verify=False).json()
     todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
                          .format(userId), verify=False).json()
 
     with open("{}.csv".format(userId), 'w') as acsvfile:
         csvwriter = csv.writer(acsvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
-            csvwriter.writerow([userId, employee_name.get('username'),
+            csvwriter.writerow([userId, emp_name.get('username'),
                                 task.get('completed'), task.get('title')])
+
 
 if __name__ == '__main__':
     userId = int(sys.argv[1])
