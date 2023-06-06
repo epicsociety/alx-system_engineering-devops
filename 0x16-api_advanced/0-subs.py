262a5ad7c4a8dc795616bddaab13f1ffc71277c0
Epicsociety <measkswho@gmail.com>
2023-06-06 21:49:57 +0300

    get the number of subscribers on a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Gets the number of subscribers of the given subreddit"""
+
+
+import requests as request
+
+
+def number_of_subscribers(subreddit):
+    """ Gets the number if subcribers """
+
+    headers = {'User-Agent': 'Custom User-Agent'}
+
+    response = request.get(f'https://www.reddit.com/r/{subreddit}/about.json',
+                   headers=headers)
+
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            total_subscribers = data['data']['subscribers']
+            return total_subscribers
+        except (KeyError, ValueError):
+            pass
+    return 0
