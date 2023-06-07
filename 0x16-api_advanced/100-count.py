86f8f0e60424fb3c9b6f05c52e0fd5a86f10299b
Epicsociety <measkswho@gmail.com>
2023-06-07 12:23:59 +0300

    more strict word search

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" module contains a recursive function that prints a word list with number of
-times it occurred"""
-
+""" module contain a recursive function that returns a key value list of words
+and count """
 
 import requests
+import re
 
 
 def count_words(subreddit, word_list, after=None, word_dict=None):
-    """ a recursive function """
+    """ a recursive function that get the count of words from titles"""
     if word_dict is None:
         word_dict = {}
 
@@ -24,15 +24,8 @@ def count_words(subreddit, word_list, after=None, word_dict=None):
         for post in posts:
             title = post['data']['title'].lower()
             for word in word_list:
-                if (
-                    word.lower() in title
-                    and not title.startswith(word.lower() + ' ')
-                    and not title.endswith(' ' + word.lower())
-                    and not title.startswith(word.lower() + '.')
-                    and not title.endswith('.' + word.lower())
-                    and not title.startswith('{' + word.lower() + '!')
-                    and not title.endswith('!' + word.lower() + '}')
-                ):
+                pattern = r'\b{}\b'.format(re.escape(word.lower()))
+                if re.search(pattern, title):
                     word_dict[word] = word_dict.get(word, 0) + 1
 
         after = data['data']['after']
@@ -40,7 +33,7 @@ def count_words(subreddit, word_list, after=None, word_dict=None):
             count_words(subreddit, word_list, after, word_dict)
         else:
             sorted_counts = sorted(word_dict.items(),
-                                   key=lambda x: (x[0].lower()))
+                                   key=lambda x: x[0].lower())
             for word, count in sorted_counts:
                 print('{}: {}'.format(word.lower(), count))
 
