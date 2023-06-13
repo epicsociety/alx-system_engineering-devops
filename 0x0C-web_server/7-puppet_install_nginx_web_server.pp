de56588da2a39bd072def713e6c546ebed05a212
epicsociety <measkswho@gmail.com>
2023-06-13 12:11:41 +0000

    applicaton server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,21 +9,14 @@ package { 'nginx':
 file { 'index.html':
     ensure  => present,
     path    => 'var/www/html/index.html',
-    content => 'Hello World!"
+    content => 'Hello World!'
 }
 
-nginx::resource::server { 'epicsociety.tech':
-    path   => '/etc/nginx/sites-available/default',
-    listen_port => '85',
-    server_name => 'epicsociety.tech',
-    location => [
-    {
-        location => '/redirect_me',
-        rewrite => [
-            rewrite "^/redirect_me" "https://www.youtube.com/watch?v=J---aiyznGQ" permanent;',
-        ],
-    },
-    ],
+file_line { 'aaaaa':
+    ensure => 'present',
+    path   => 'etc/nginx/sites-available/default',
+    after  => 'listen 80 default_server;',
+    line   =>  "rewrite \"^/redirect_me\" \"https://www.youtube.com/watch?v=J---aiyznGQ\" permanent;",
 }
 
 service { 'nginx':
