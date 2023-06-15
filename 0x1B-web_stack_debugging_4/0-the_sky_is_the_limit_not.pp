34992df2573ecee00dc0696f68585030f466598f
Epicsociety <measkswho@gmail.com>
2023-06-16 01:55:32 +0300

    Web-stack debugging guru

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Allows for multiple concurrent requests on nginx
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
